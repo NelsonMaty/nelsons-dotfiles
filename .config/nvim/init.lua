37358c99b5eb911ec91fe417c0c3ef5bf4961b07
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-20 00:01:43 -0300

    feat(nvim): remap find_files to space leader key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,11 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+
 require('core.bootstrap')
 
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader> ', builtin.find_files, {})
