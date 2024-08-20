17e13ba091ed064caa3491d9e1f972f632971bfc
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-19 22:06:20 -0300

    feat: add Dracula theme to Neovim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,29 @@
 return {
+    -- Dracula color scheme
+    {
+        'Mofiqul/dracula.nvim',  -- or 'maxmx03/dracula.nvim' depending on your preference
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('dracula').setup({
+                -- Optional configurations
+                transparent = false,  -- Set to true if you want a transparent background
+                styles = {
+                    comments = { italic = true },
+                    functions = { bold = true },
+                    keywords = { italic = true },
+                },
+                on_colors = function(colors)
+                    -- Customize colors if needed
+                end,
+                on_highlights = function(highlights)
+                    -- Customize highlights if needed
+                end,
+            })
+            vim.cmd.colorscheme('dracula')  -- Set the colorscheme
+        end
+    },
+
     -- Status line
     { 'nvim-lualine/lualine.nvim', lazy = false },
 }
