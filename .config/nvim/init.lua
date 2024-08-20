47be9d25b8f563477988780ec45ce9416d680ee4
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-20 00:05:23 -0300

    feat(nvim): add Telescope keybindings for file navigation and search

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,4 +10,11 @@ vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true 
 require('core.bootstrap')
 
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader> ', builtin.find_files, {})
+local keymap = vim.keymap
+
+keymap.set('n', '<leader> ', builtin.find_files, { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find string in files" })
+keymap.set("n", "<leader>fc", "<cmd>Telescope git_commits<cr>", { desc = "Browse git commits" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
+keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Search for the word under cursor" })
