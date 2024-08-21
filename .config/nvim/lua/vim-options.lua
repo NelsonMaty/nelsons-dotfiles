9524047187fae44c6c96670d57cb70380ca5469c
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-20 21:01:34 -0300

    feat(nvim): set default indentation to 2 spaces

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,6 +3,12 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
+-- Editor default options
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 -- Set leader keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
