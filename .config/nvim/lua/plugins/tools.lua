70f3e169cf19ae0e957b8a7043ef5210335f5f81
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-21 01:17:41 -0300

    refactor(telescope): reorganize telescope config into separate file

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -11,40 +11,6 @@ return {
       })
     end
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },     -- Required dependency
-    cmd = "Telescope",                              -- Load on command
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          -- Default configuration options
-          file_ignore_patterns = { "^.git/", "^node_modules/" },
-        },
-        pickers = {
-          live_grep = {
-            additional_args = function(opts)
-              return { '--hidden' }
-            end
-          },
-          grep_string = {
-            additional_args = function(opts)
-              return { '--hidden' }
-            end
-          },
-          -- Configuration for built-in pickers
-          find_files = {
-            hidden = true,
-          },
-        },
-        extensions = {
-          -- Load any extensions here
-        },
-      })
-      -- Load any extensions you want to use
-      -- require("telescope").load_extension("your_extension")
-    end,
-  },
   -- LazyGit
   {
     "kdheepak/lazygit.nvim",
