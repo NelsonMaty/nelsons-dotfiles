b282a753a60419900e186bac8a8e23066a1daf4e
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-21 18:48:13 -0300

    feat: add todo-comments plugin to disabled list

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
 }
