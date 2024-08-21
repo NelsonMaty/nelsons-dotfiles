4ee17008b7fa142aa604e8eaf18cf94eb9905f42
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-21 10:49:36 -0300

    feat(nvim): integrate none-ls for formatting with prettier

---
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,9 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      null_ls.builtins.formatting.prettier,
+    })
+  end
+}
