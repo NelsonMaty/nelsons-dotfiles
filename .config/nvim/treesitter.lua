18db0b77637b3e7a9d0a64a32f67ce7e0b4c3ed3
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-21 10:49:08 -0300

    feat(nvim): add none-ls plugins and expand treesitter configs

--- .config/nvim/treesitter.lua
+++ .config/nvim/treesitter.lua
@@ -1,6 +1,31 @@
-local config = require('nvim-treesitter.configs')
+local config = require("nvim-treesitter.configs")
 config.setup({
-  ensure_installed = { "lua", "bash" },
+  auto_install = true,
+  ensure_installed = {
+    "lua",
+    "bash",
+    "json",
+    "javascript",
+    "typescript",
+    "tsx",
+    "html",
+    "css",
+    "yaml",
+    "markdown",
+    "vim",
+    "dockerfile",
+    "gitignore",
+  },
   highlight = { enable = true },
+  rainbow = { enable = true, disable = { "html" } },
   indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
 })
