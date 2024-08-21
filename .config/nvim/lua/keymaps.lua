3498e1c2c31535cf83c250fb263c64604153ed37
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-20 21:12:59 -0300

    refactor(nvim): centralize keymaps in separate file

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,13 @@
+-- ~/.config/nvim/lua/keymaps.lua
+
+local builtin = require("telescope.builtin")
+local keymap = vim.keymap
+
+keymap.set('n', '<leader> ', builtin.find_files, { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find string in files" })
+keymap.set("n", "<leader>fc", "<cmd>Telescope git_commits<cr>", { desc = "Browse git commits" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
+keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Search for the word under cursor" })
+keymap.set("n", "-", require('oil').open, { desc = "Open parent directory" })
+keymap.set("n", "<leader>q", require('oil').close, { desc = "Close Oil buffer" })
