5d878b3d0fe5f697118ec1ac35850c74d855ad3f
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-21 23:48:04 -0300

    refactor(mini.indentscope): replace unicode with ASCII symbol, remove guard

--- .config/nvim/lua/plugins/mini.indentscope.lua
+++ .config/nvim/lua/plugins/mini.indentscope.lua
@@ -1,13 +1,10 @@
-if true then
-  return {}
-end
 return {
   "echasnovski/mini.indentscope",
   version = false, -- wait till new 0.7.0 release to put it back on semver
   event = "LazyFile",
   opts = {
     -- symbol = "▏",
-    symbol = "│",
+    symbol = "|",
     options = { try_as_border = true },
   },
   init = function()
