bd441b4fee714a54dfad0caafb86e30651aac942
NelsonMaty <nelsonmatias9@hotmail.com>
2024-08-30 21:47:59 -0300

    feat: add macro recording status to lualine_z section

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,14 @@
 -- Author: lokesh-krishna
 -- MIT license, see LICENSE for more details.
 
+local function macro_recording()
+  local reg = vim.fn.reg_recording()
+  if reg == "" then
+    return "" -- Not recording
+  end
+  return "Recording to @" .. reg -- Display the register being recorded
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
@@ -18,7 +26,7 @@ return {
       },
       lualine_x = { "diagnostics" },
       lualine_y = {},
-      lualine_z = {},
+      lualine_z = { macro_recording },
     },
     inactive_sections = {
       lualine_a = { "filename" },
