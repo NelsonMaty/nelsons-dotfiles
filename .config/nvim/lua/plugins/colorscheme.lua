cc852f3098474ac94972dda3d2f3c645e2363387
NelsonMaty <nelsonmatias9@hotmail.com>
2024-09-03 19:52:32 -0300

    - feat(colorscheme): enable transparent bg for dracula.nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,11 @@
 return {
   -- add dracula
-  { "Mofiqul/dracula.nvim" },
-
+  {
+    "Mofiqul/dracula.nvim",
+    opts = {
+      transparent_bg = true,
+    },
+  },
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
