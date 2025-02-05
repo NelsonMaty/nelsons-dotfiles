2a9dcc1d12be8d59688e12e9185b87eac55d3a87
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-05 15:26:53 -0300

    feat: replace neo-tree with oil.nvim

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    -- Optional configuration
+    default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
+
+    prompt_save_on_select_new_entry = false,
+  },
+  config = function(_, opts)
+    require("oil").setup(opts)
+  end,
+}
