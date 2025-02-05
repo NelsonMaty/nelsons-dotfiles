9a6918a0f0347b47d5f3722df2068bf946b2c8ab
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-05 15:06:24 -0300

    feat: add dracula theme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- Add dracula colorscheme
+  { "Mofiqul/dracula.nvim" },
+  -- Configure nvim to load dracula
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
