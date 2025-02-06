7126a1082ed89df7e5a03839757b3f8c3a1ec264
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-06 12:55:09 -0300

    feat: add snipe for buffer navigation

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>bD", ":%bd|bd#<cr>", { desc = "Close all buffers including current" })
+vim.keymap.set("n", "<leader>sb", require("snipe").open_buffer_menu, { desc = "Open Snipe buffer menu" })
