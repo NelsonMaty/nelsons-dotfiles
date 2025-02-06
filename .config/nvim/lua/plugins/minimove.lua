a5de99cb03c95998292345b53fa653c23c6cb21e
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-06 13:38:48 -0300

    feat: Add mini.move plugin for enhanced text movement in Neovim

---
+++ .config/nvim/lua/plugins/minimove.lua
@@ -0,0 +1,13 @@
+return {
+  "echasnovski/mini.move",
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      -- Move selection in visual mode
+      line_left = "<S-h>",
+      line_right = "<S-l>",
+      line_down = "<S-j>",
+      line_up = "<S-k>",
+    },
+  },
+}
