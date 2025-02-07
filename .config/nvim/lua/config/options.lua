1e857fe97fd84523d666cd5dcc97bbd8d881c411
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-07 20:56:17 -0300

    feat: Configure cursor style and enable smear between neighbor lines

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,6 @@
 -- Add any additional options here
 vim.g.lazyvim_prettier_needs_config = false
 vim.g.lazyvim_eslint_auto_format = false
+
+vim.opt.guicursor =
+  "n-v-c-sm:block-blinkwait175-blinkoff150-blinkon175,i-ci-ve:ver25-blinkwait175-blinkoff150-blinkon175,r-cr-o:hor20"
--- .config/nvim/lua/plugins/smearcursor.lua
+++ .config/nvim/lua/plugins/smearcursor.lua
@@ -2,6 +2,6 @@ return {
   "sphamba/smear-cursor.nvim",
 
   opts = {
-    smear_between_neighbor_lines = false,
+    smear_between_neighbor_lines = true,
   },
 }
