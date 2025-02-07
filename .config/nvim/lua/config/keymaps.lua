ff67ad0c6d3270898b6106ea201971715e7ee93b
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-07 02:27:27 -0300

    feat: add window split keymaps and remove tmux sessionx plugin

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,13 @@
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>bD", ":%bd|bd#<cr>", { desc = "Close all buffers including current" })
 vim.keymap.set("n", "<leader>sb", require("snipe").open_buffer_menu, { desc = "Open Snipe buffer menu" })
+
+-- Split windows
+vim.keymap.set("n", "ss", ":split<Return>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "sv", ":vsplit<Return>", { desc = "Split window vertically" })
+
+-- Do things without affecting the registers
+vim.keymap.set("n", "x", '"_x')
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all text in current document" })
