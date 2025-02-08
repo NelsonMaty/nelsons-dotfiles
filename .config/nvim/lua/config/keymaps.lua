e5da360633ea049caa8b70844bfdaae478475f9a
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-07 21:24:50 -0300

    chore: Refactored keymaps for simpler use

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,16 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
-vim.keymap.set("n", "<leader>bD", ":%bd|bd#<cr>", { desc = "Close all buffers including current" })
-vim.keymap.set("n", "<leader>sb", require("snipe").open_buffer_menu, { desc = "Open Snipe buffer menu" })
+local normal_mode_keybinds = {
+  { key = "-", command = require("oil").open, opts = { desc = "Open parent directory" } },
+  { key = "<leader>bD", command = ":%bd|bd#<cr>", opts = { desc = "Close all buffers including current" } },
+  { key = "<leader>sb", command = require("snipe").open_buffer_menu, opts = { desc = "Open Snipe buffer menu" } },
+  { key = "ss", command = ":split<Return>", opts = { desc = "Split window horizontally" } },
+  { key = "sv", command = ":vsplit<Return>", opts = { desc = "Split window vertically" } },
+  { key = "x", command = '"_x', opts = {} },
+  { key = "<C-a>", command = "gg<S-v>G", opts = { desc = "Select all text in current document" } },
+}
 
--- Split windows
-vim.keymap.set("n", "ss", ":split<Return>", { desc = "Split window horizontally" })
-vim.keymap.set("n", "sv", ":vsplit<Return>", { desc = "Split window vertically" })
-
--- Do things without affecting the registers
-vim.keymap.set("n", "x", '"_x')
-
--- Select all
-vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all text in current document" })
+for _, map in ipairs(normal_mode_keybinds) do
+  vim.keymap.set("n", map.key, map.command, map.opts)
+end
