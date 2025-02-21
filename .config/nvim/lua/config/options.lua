1861ce9e83abff4811ae999dc024b2e0611f6475
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-21 18:14:17 -0300

    refactor: Simplify avante.nvim config and add img-clip plugin

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,9 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.lazyvim_prettier_needs_config = false
+vim.g.lazyvim_prettier_needs_config = true
 vim.g.lazyvim_eslint_auto_format = false
+
+vim.g.snacks_animate = false
 
 vim.o.mouse = ""
 vim.opt.guicursor =
   "n-v-c-sm:block-blinkwait175-blinkoff150-blinkon175,i-ci-ve:ver25-blinkwait175-blinkoff150-blinkon175,r-cr-o:hor20"
+
+vim.g.lazyvim_mini_snippets_in_completion = true
