70fb8bf47e5594ceb4fff8c9877eb46bc2d88035
NelsonMaty <nelsonmatias9@hotmail.com>
2025-02-21 18:13:29 -0300

    chore: refactor keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,43 +2,30 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local normal_mode_keybinds = {
-  { "-", require("oil").open, { desc = "Open parent directory" } },
-  { "<leader>bD", ":%bd|bd#<cr>", { desc = "Close all buffers including current" } },
-  { "<leader>ss", ":split<Return>", { desc = "Split window horizontally" } },
-  { "<leader>sv", ":vsplit<Return>", { desc = "Split window vertically" } },
-  { "x", '"_x', {} },
-  { "<C-a>", "gg<S-v>G", { desc = "Select all text in current document" } },
-  {
-    "<leader>tt",
-    require("twilight").toggle,
-    { desc = "Toggle Twilight" },
-  },
-  {
-    "<leader>e",
-    function()
-      require("mini.files").open(vim.api.nvim_buf_get_name(0), false)
-    end,
-    { desc = "Open Mini file explorer" },
-  },
-}
+vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>bD", ":%bd|bd#<cr>", { desc = "Close all buffers including current" })
+vim.keymap.set("n", "<leader>ss", ":split<Return>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>sv", ":vsplit<Return>", { desc = "Split window vertically" })
+vim.keymap.set("n", "x", '"_x', {})
+vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all text in current document" })
+vim.keymap.set("n", "<leader>tt", require("twilight").toggle, { desc = "Toggle Twilight" })
+vim.keymap.set("n", "<leader>e", function()
+  require("mini.files").open(vim.api.nvim_buf_get_name(0), false)
+end, { desc = "Open Mini file explorer" })
+vim.keymap.set(
+  "n",
+  "<leader><space>",
+  LazyVim.pick("files", { root = false }),
+  { desc = "Find Files (cwd)", remap = true }
+)
 
 vim.keymap.set("n", "<leader>cp", function()
   local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
   local file_path = vim.fn.expand("%:p")
   local relative_path = file_path:sub(#git_root + 2) -- Trim the Git root + '/'
-  vim.fn.setreg("+", relative_path)
-  print("Copied: " .. relative_path)
+  local result_path = "/add " .. relative_path
+  vim.fn.setreg("+", result_path)
+  print("Copied: " .. result_path)
 end, { silent = true })
 
-local insert_mode_keybinds = {
-  { "jj", "<Esc>", { desc = "Exit insert mode" } },
-}
-
-for _, map in ipairs(normal_mode_keybinds) do
-  vim.keymap.set("n", map[1], map[2], map[3])
-end
-
-for _, map in ipairs(insert_mode_keybinds) do
-  vim.keymap.set("i", map[1], map[2], map[3])
-end
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
