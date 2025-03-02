56e175352f149340c3ba87e281b60722508b708f
NelsonMaty <nelsonmatias9@hotmail.com>
2025-03-02 15:27:38 -0300

    chore: update config files and remove unused Neovim plugins

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -5,10 +5,12 @@ return {
     -- Optional configuration
     default_file_explorer = true,
     skip_confirm_for_simple_edits = true,
+    delete_to_trash = true,
     view_options = {
       show_hidden = true,
     },
     prompt_save_on_select_new_entry = false,
+    skip_confirm_for_simple_edits = true,
   },
   config = function(_, opts)
     require("oil").setup(opts)
